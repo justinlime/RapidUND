0d3035ac8d4bdb3a76cffee6f5de48d163b2b004
Justin Fields <justinlime1999@gmail.com>
2022-11-27 22:11:36 -0600

    setting up

--- undsetup.sh
+++ undsetup.sh
@@ -2,6 +2,7 @@
 GENESIS=https://raw.githubusercontent.com/unification-com/mainnet/master/latest/genesis.json
 GENESIS_UND=https://github.com/unification-com/mainchain/releases/download/1.5.1/und_v1.5.1_linux_x86_64.tar.gz
 TAR_UND=und_v1.6.3_linux_x86_64.tar.gz
+TAR_GENESIS_UND=und_v1.5.1_linux_x86_64.tar.gz
 UND=https://github.com/unification-com/mainchain/releases/download/v1.6.3/und_v1.6.3_linux_x86_64.tar.gz
 COSMOVISOR=https://github.com/cosmos/cosmos-sdk/releases/download/cosmovisor%2Fv1.2.0/cosmovisor-v1.2.0-linux-amd64.tar.gz
 TAR_COSMOVISOR=cosmovisor-v1.2.0-linux-amd64.tar.gz
@@ -13,21 +14,34 @@ TAR_COSMOVISOR=cosmovisor-v1.2.0-linux-amd64.tar.gz
 # read -p "Select pruning option> [default][nothing][custom]" pruning
 read -p "Input node moniker> " MONIKER
 
+#Removing old directories and files
 sudo rm -r $HOME/.und_mainchain
 sudo rm -r $HOME/temp
 sudo rm -r $HOME/UNDBackup
 sudo rm -r /usr/local/bin/und
 sudo rm -r /etc/systemd/system/und.service
 
-mkdir $HOME/temp
-mkdir $HOME/temp/und
-mkdir $HOME/temp/genesis_und
+#Making new working directories
 mkdir $HOME/UNDBackup
+mkdir $HOME/temp
+mkdir $HOME/temp/main_und
+mkdir $HOME/temp/genesis_und
+mkdir $HOME/temp/cosmovisor
 
-wget $UND
-mv $TAR_UND $HOME/temp
-tar -zxvf $HOME/temp/$TAR_UND -C $HOME/temp
-sudo mv $HOME/temp/und /usr/local/bin
+#Setting up UND
+wget $UND -P $HOME/temp/main_und
+tar -zxvf $HOME/temp/main_und/$TAR_UND -C $HOME/temp/main_und
+sudo mv $HOME/temp/main_und/und /usr/local/bin
 und init $MONIKER
 curl $GENESIS > $HOME/.und_mainchain/config/genesis.json
 #sed -i 's/trust_height = 0/trust_height = 200/' config.toml
+
+#Setting up Cosmovisor
+wget $COSMOVISOR -P $HOME/temp/cosmovisor
+wget $GENESIS_UND -P $HOME/temp/genesis_und
+tar -zxvf $HOME/temp/genesis_und/$TAR_GENESIS_UND -C $HOME/temp/genesis_und
+tar -zxvf $HOME/temp/cosmovisor/$TAR_COSMOVISOR -C $HOME/temp/cosmovisor
+mkdir -p $HOME/.und_mainchain/cosmovisor/genesis/bin
+mv $HOME/temp/genesis_und/und $HOME/.und_mainchain/cosmovisor/genesis/bin
+sudo mv $HOME/temp/cosmovisor/cosmovisor /usr/local/bin
+
