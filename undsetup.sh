54e9bb099bc98ea860416129c67ab1407a5f2fdb
justinlime <justinlime1999@gmail.com>
2023-01-10 22:07:59 -0600

    Added Testnet and Remove node options

--- undsetup.sh
+++ undsetup.sh
@@ -2,18 +2,82 @@
 GENESIS=https://raw.githubusercontent.com/unification-com/mainnet/master/latest/genesis.json
 UND=https://github.com/unification-com/mainchain/releases/download/v1.6.3/und_v1.6.3_linux_x86_64.tar.gz
 COSMOVISOR=https://github.com/cosmos/cosmos-sdk/releases/download/cosmovisor%2Fv1.2.0/cosmovisor-v1.2.0-linux-amd64.tar.gz
+SEEDS=0c2b65bc604a18a490f5f55bb7b4140cfb512ced@seed1.unification.io:26656,e66e0f89af19da09f676c85b262d591b8c2bb9d8@seed2.unification.io:26656
+RPC=sync1.unification.io:26657,sync2.unification.io:26657
+HANDH=$(curl -s https://rest.unification.io/blocks/latest | jq '.|[.block_id.hash,.block.header.height]')
 
-echo -e "\n!!!CAUTION!!! This script will DESTROY the following files if present: \n\n$HOME/.und_mainchain\n$HOME/temp\n$HOME/UNDBackup\n/usr/local/bin/und\n/usr/local/bin/cosmovisor\n/etc/systemd/system/und.service\n"
+remove_und() {
+        sudo systemctl daemon-reload
+	sudo systemctl stop und
+	sudo systemctl disable und
+	sudo rm -r $HOME/.und_mainchain
+	sudo rm -r $HOME/temp
+	sudo rm -r $HOME/UNDBackup
+	sudo rm -r /usr/local/bin/und
+	sudo rm -r /usr/local/bin/cosmovisor
+	sudo rm -r /etc/systemd/system/und.service
+}
+
+echo -e "\nThis script is made to rapidly deploy a Unification Node, for Testnet or Mainnet, intended for expirienced operators to rapidly redeploy. If there are any issues please report them on github.\n"
 
 while true;
 do
-read -p "Do you wish to continue?[y/n]> " CHOOSE
-if [ "$CHOOSE" = "n" ];
+read -p "Install, or Remove the node?[i/r]> " CHOOSE
+if [ "$CHOOSE" = "i" ];
 then
-    echo "Exiting"
+     break
+fi
+if [ "$CHOOSE" = "r" ];
+then
+     while true;
+     do
+     echo -e "\n!!!CAUTION!!!\n\nThis option will DESTROY the following files if present: \n\n$HOME/.und_mainchain\n$HOME/temp\n$HOME/UNDBackup\n/usr/local/bin/und\n/usr/local/bin/cosmovisor\n/etc/systemd/system/und.service\n"
+     read -p "Do you wish to continue?[y/n]> " CONTINUE
+     if [ "$CONTINUE" = "y" ];
+     then
+          remove_und
+          echo -e "\nAll Unification files removed\n"
+          exit
+     fi
+     if [ "$CONTINUE" = "n" ];
+     then
+	  echo -e "\nExiting\n"
+          exit
+     fi        
+     done
+fi
+done 
+
+echo -e "\n!!!CAUTION!!!\n\nThis script will DESTROY the following files if present: \n\n$HOME/.und_mainchain\n$HOME/temp\n$HOME/UNDBackup\n/usr/local/bin/und\n/usr/local/bin/cosmovisor\n/etc/systemd/system/und.service\n"
+
+while true;
+do
+read -p "Do you wish to continue?[y/n]> " CONTINUE
+if [ "$CONTINUE" = "n" ];
+then
+    echo -e "\nExiting\n"
     exit
 fi
-if [ "$CHOOSE" = "y" ];
+if [ "$CONTINUE" = "y" ];
+then
+    break
+fi
+done
+
+while true;
+do
+read -p "Would you like to set up for Testnet, or Mainnet?[t/m]> " TEST_MAIN
+if [ "$TEST_MAIN" = "t" ];
+then
+     GENESIS=https://raw.githubusercontent.com/unification-com/testnet/master/latest/genesis.json
+     UND=https://github.com/unification-com/mainchain/releases/download/v1.6.3/und_v1.6.3_linux_x86_64.tar.gz
+     COSMOVISOR=https://github.com/cosmos/cosmos-sdk/releases/download/cosmovisor%2Fv1.2.0/cosmovisor-v1.2.0-linux-amd64.tar.gz
+     SEEDS=e428c5653865da4a55f5599d7ff3203789abadfd@seed-testnet.unification.io:26656
+     RPC=sync1-testnet.unification.io:26657,sync2-testnet.unification.io:26657
+     HANDH=$(curl -s https://rest-testnet.unification.io/blocks/latest | jq '.|[.block_id.hash,.block.header.height]')
+     break
+fi
+if [ "$TEST_MAIN" = "m" ];
 then
     break
 fi
@@ -22,15 +86,7 @@ done
 read -p "Input node moniker/name> " MONIKER
 
 #Removing old directories and files
-sudo systemctl daemon-reload
-sudo systemctl stop und
-sudo systemctl disable und
-sudo rm -r $HOME/.und_mainchain
-sudo rm -r $HOME/temp
-sudo rm -r $HOME/UNDBackup
-sudo rm -r /usr/local/bin/und
-sudo rm -r /usr/local/bin/cosmovisor
-sudo rm -r /etc/systemd/system/und.service
+remove_und
 
 #Making new working directories
 mkdir $HOME/UNDBackup
@@ -79,19 +135,22 @@ DAEMON_RESTART_DELAY=5s
 EOF
 
 #Editing configs
-HANDH=$(curl -s https://rest.unification.io/blocks/latest | jq '.|[.block_id.hash,.block.header.height]')
 HASH="${HANDH:4:66}"
 HEIGHT="${HANDH:75:7}"
 sed -i "s/enable = false/enable = true/" $HOME/.und_mainchain/config/config.toml
 sed -i "s/trust_height = 0/trust_height = $HEIGHT/" $HOME/.und_mainchain/config/config.toml
 sed -i 's/trust_hash = ""/trust_hash = '"$HASH"'/' $HOME/.und_mainchain/config/config.toml
-sed -i 's/rpc_servers = ""/rpc_servers = "sync1.unification.io:26657,sync2.unification.io:26657"/' $HOME/.und_mainchain/config/config.toml
+sed -i 's/rpc_servers = ""/rpc_servers = "'$RPC'"/' $HOME/.und_mainchain/config/config.toml
 sed -i 's/discovery_time = "15s"/discovery_time = "30s"/' $HOME/.und_mainchain/config/config.toml
 sed -i 's/chunk_request_timeout = "10s"/chunk_request_timeout = "60s"/' $HOME/.und_mainchain/config/config.toml
-sed -i 's/seeds = ""/seeds = "0c2b65bc604a18a490f5f55bb7b4140cfb512ced@seed1.unification.io:26656,e66e0f89af19da09f676c85b262d591b8c2bb9d8@seed2.unification.io:26656"/' $HOME/.und_mainchain/config/config.toml
+sed -i 's/seeds = ""/seeds = "'$SEEDS'"/' $HOME/.und_mainchain/config/config.toml
 
 #Cleanup
 rm -r $HOME/temp
+
+#SELinux Fix
+
+
 
 #Startup
 sudo systemctl daemon-reload
